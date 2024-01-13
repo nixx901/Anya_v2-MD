e5ac4f268c3b052c70af76152471675403b2d6af
nixx901 <137382002+nixx901@users.noreply.github.com>
2024-01-13 14:03:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -70,16 +70,16 @@ module.exports = {
   footer: process.env.Footer || "© Queen Anya Bot",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "亗一『Ｐｒｉｍｅ』・",
-  ownernumber: process.env.Owner_Number || "916001031685",
+  ownername: process.env.Owner_Name || "Pika~Kun",
+  ownernumber: process.env.Owner_Number || "916900904828",
   instagramId: process.env.Insta || "8.08_only_mine",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "mongodb+srv://rush:20041025sa@cluster0.ysi6dsf.mongodb.net/",
+  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "dFJoeGR_Queen-Anya_DTGY=", 
+  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
@@ -112,8 +112,8 @@ global.myweb = process.env.Web || "https://github.com/PikaBotz"
 global.footer = process.env.Footer || "© Queen Anya Bot" 
 global.prefa = process.env.Prefix || ['-'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "亗一『Ｐｒｉｍｅ』・" 
-global.ownernumber = process.env.Owner_Number || "916001031685" 
+global.ownername = process.env.Owner_Name || "Pika~Kun" 
+global.ownernumber = process.env.Owner_Number || "916900904828" 
 global.adress = process.env.Continent || "Asia, India, Assam" 
 global.timezone = process.env.TimeZone || "Asia/Kolkata" 
 global.instagramId = process.env.Insta || "8.08_only_mine" 
