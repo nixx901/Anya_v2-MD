46ddc010a8889d802cc78438ff6b1507040627a5
nixx901 <137382002+nixx901@users.noreply.github.com>
2024-02-19 11:00:00 +0530

    config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ module.exports = {
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
+  sessionId: process.env.SESSION_ID || "WGR4Q1RCYUwjenN_Queen-Anya_jSmxuU29haHQ1M1JiOHN_Queen-Anya_HN_Queen-Anya_zBiT3JiMlN_Queen-Anya_iSnhPcXg2dnpQdmlPdjB1TQ==", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
